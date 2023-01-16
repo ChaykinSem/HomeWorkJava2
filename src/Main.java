599d6ff63221c6322869e1a7143cb54117ddb35e
Semen Vitalich <Chaykin96@gmail.com>
2023-01-16 17:48:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 600; // Цена 1-го билета в рублях
+
+        int y = 20; // Сумма рублей за 1 бонусную милю
+
+        int bonus = x / y;
+
+        System.out.println("Количество бонусных миль за билет:" +  bonus);
+
+    }
+}
